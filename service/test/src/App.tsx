2e8534b6ed0b6fe1ffcd87e53159d3e5aac802d7
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-04-03 23:16:41 +0900

    Test 프로젝트를 통해 디자인 토큰이 잘 동작하는지 확인

---
+++ service/test/src/App.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ThemeProvider } from "@emotion/react";
+import { vars } from '@toytoy/themes';
+import styled from "@emotion/styled";
+
+function App() {
+  const theme = {
+    colors: vars.colors.$static.light
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <View />
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
+const View = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Text>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </Text>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+const Text = styled.p`
+  color: ${({ theme }) => {
+    // @ts-ignore
+    return theme.colors.red[900];
+  }};
+`

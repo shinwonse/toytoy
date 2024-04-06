f71e56cfb2cc36eb300a5ddf380f877b0f56b2f2
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-04-06 21:30:47 +0900

    공통 esbuild config 설정

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,38 +1,4 @@
-import esbuild from 'esbuild';
+import run from '@toytoy/esbuild-config';
 import pkg from './package.json' assert { type: 'json' };
 
-const dev = process.argv.includes('--dev');
-const minify = !dev;
-
-const watch = process.argv.includes('--watch');
-
-const external = Object.keys({
-  ...pkg.devDependencies,
-  ...pkg.peerDependencies,
-});
-
-const baseConfig = {
-  entryPoints: ['src/index.ts'],
-  bundle: true,
-  minify,
-  sourcemap: true,
-  outdir: 'dist',
-  target: 'es2019',
-  watch,
-  external,
-};
-
-Promise.all([
-  esbuild.build({
-    ...baseConfig,
-    format: 'esm',
-  }),
-  esbuild.build({
-    ...baseConfig,
-    outExtension: { '.js': '.cjs' },
-  })
-]).catch(() => {
-  console.error('Build failed');
-  process.exit(1);
-});
-
+run({ pkg });
